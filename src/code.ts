742e6a8963cf0954896457f99fd6faad276eb677
Oleg Baltag <online7890@gmail.com>
2023-02-23 19:18:26 +0200

    update color scheme & add dark mode support

--- src/code.ts
+++ src/code.ts
@@ -6,7 +6,9 @@ import { onResize } from "./actions/onResize";
 
 const init = async () => {
   // This shows the HTML page in "ui.html".
-  figma.showUI(__html__);
+  figma.showUI(__html__, {
+    themeColors: true,
+  });
 
   // Calls to "parent.postMessage" from within the HTML page will trigger this
   // callback. The callback will be passed the "pluginMessage" property of the
